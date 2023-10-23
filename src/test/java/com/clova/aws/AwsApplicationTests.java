d9d182d8acbdfb52374384c8923cef791a5213a8
hseong3243 <hseong3243@gmail.com>
2023-10-23 22:40:26 +0900

    git init

---
+++ src/test/java/com/clova/aws/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clova.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
