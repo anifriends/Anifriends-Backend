d516dabee80ca2cc5fe34c06fcd2d5edd2a85668
hseong3243 <hseong3243@gmail.com>
2023-10-25 10:39:23 +0900

    test: BaseControllerTest에 RestDocs 문서화 설정 적용 (#2)

--- anifriends/src/test/java/com/clova/anifriends/base/BaseControllerTest.java
+++ anifriends/src/test/java/com/clova/anifriends/base/BaseControllerTest.java
@@ -4,15 +4,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 @WebMvcTest
-public class BaseControllerTest {
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class BaseControllerTest {
 
     protected MockMvc mockMvc;
 
@@ -20,9 +25,12 @@ public class BaseControllerTest {
     protected ObjectMapper objectMapper;
 
     @BeforeEach
-    void setUp(WebApplicationContext context) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+    void setUp(
+        WebApplicationContext applicationContext,
+        RestDocumentationContextProvider documentationContextProvider) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext)
             .alwaysDo(print())
+            .apply(MockMvcRestDocumentation.documentationConfiguration(documentationContextProvider))
             .addFilter(new CharacterEncodingFilter("UTF-8", true))
             .build();
     }
