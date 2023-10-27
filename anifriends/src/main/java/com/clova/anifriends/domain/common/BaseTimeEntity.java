e77826a3161a544bf81a8e62f578d597c2bb6c5d
pushedrumex <pushedrumex@naver.com>
2023-10-27 22:47:33 +0900

    feat: BaseTimeEntity 생성

---
+++ anifriends/src/main/java/com/clova/anifriends/domain/common/BaseTimeEntity.java
@@ -0,0 +1,18 @@
+package com.clova.anifriends.domain.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "createdAt", updatable = false)
+    private LocalDateTime createdAt;
+
+}
