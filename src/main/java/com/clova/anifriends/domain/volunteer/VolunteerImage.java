dec4ffa486be5222905ce5b9c54b316a11ccdd43
hseong3243 <hseong3243@gmail.com>
2023-10-28 12:44:09 +0900

    fix: 원활한 테스트를 위한 VolunteerImage 엔티티 `@Table(name)` 추가

--- src/main/java/com/clova/anifriends/domain/volunteer/VolunteerImage.java
+++ src/main/java/com/clova/anifriends/domain/volunteer/VolunteerImage.java
@@ -9,8 +9,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "voulunteer_image")
 public class VolunteerImage extends BaseTimeEntity {
 
     @Id
