df849aa018bffd590a931d67d17544a337b2e53b
Sunny <rosa7536@naver.com>
2023-10-31 23:45:47 +0900

    feat: 에러 코드를 추가한다.

--- src/main/java/com/clova/anifriends/global/exception/ErrorCode.java
+++ src/main/java/com/clova/anifriends/global/exception/ErrorCode.java
@@ -8,6 +8,20 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ErrorCode {
 
-    
+    // 400
+    AF001, // 잘못된 입력값
+    AF002, // 아이디 비밀번호 매치 안됨
+    // 401
+    AF101, // 토큰 만료
+    AF102, // 인증 안됨
+    // 403
+    AF301, // 권한 없음
+    // 404
+    AF401, // 존재하지 않는 리소스
+    // 409
+    AF901, // 이미 존재하는 리소스
+    AF902, // 선착순 마감
+    // 500
+    AF999; // 서버 내부 에러
 
 }
