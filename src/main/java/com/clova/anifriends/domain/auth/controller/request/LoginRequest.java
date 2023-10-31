03d41cf0afdc6b652f50c2a064a5a0e6b7632c2e
hseong3243 <hseong3243@gmail.com>
2023-11-01 02:08:38 +0900

    feat: 로그인 api 요청 바디가 공백일 시 반환하는 메시지를 추가한다.

--- src/main/java/com/clova/anifriends/domain/auth/controller/request/LoginRequest.java
+++ src/main/java/com/clova/anifriends/domain/auth/controller/request/LoginRequest.java
@@ -3,9 +3,9 @@ package com.clova.anifriends.domain.auth.controller.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest(
-    @NotBlank
+    @NotBlank(message = "이메일은 공백일 수 없습니다.")
     String email,
-    @NotBlank
+    @NotBlank(message = "패스워드는 공백일 수 없습니다.")
     String password) {
 
 }
