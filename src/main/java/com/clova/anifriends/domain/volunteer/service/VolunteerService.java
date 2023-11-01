efaf8be81cb61b8334b34944a072a1bdd899f69e
Sunny <rosa7536@naver.com>
2023-11-01 10:44:31 +0900

    feat: 봉사자 마이페이지 조회 서비스단을 작성한다.

--- src/main/java/com/clova/anifriends/domain/volunteer/service/VolunteerService.java
+++ src/main/java/com/clova/anifriends/domain/volunteer/service/VolunteerService.java
@@ -2,6 +2,8 @@ package com.clova.anifriends.domain.volunteer.service;
 
 import com.clova.anifriends.domain.volunteer.Volunteer;
 import com.clova.anifriends.domain.volunteer.dto.request.RegisterVolunteerRequest;
+import com.clova.anifriends.domain.volunteer.dto.response.GetVolunteerMyPageResponse;
+import com.clova.anifriends.domain.volunteer.exception.VolunteerNotFoundException;
 import com.clova.anifriends.domain.volunteer.repository.VolunteerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -28,4 +30,13 @@ public class VolunteerService {
         return volunteer.getVolunteerId();
     }
 
+    @Transactional(readOnly = true)
+    public GetVolunteerMyPageResponse getVolunteerMyPage(Long volunteerId) {
+        return GetVolunteerMyPageResponse.from(getVolunteer(volunteerId));
+    }
+
+    private Volunteer getVolunteer(Long volunteerId) {
+        return volunteerRepository.findById(volunteerId)
+            .orElseThrow(() -> new VolunteerNotFoundException("존재하지 않는 봉사자입니다."));
+    }
 }
