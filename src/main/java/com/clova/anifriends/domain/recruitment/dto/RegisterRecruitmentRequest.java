d09423c160eaabe13d049995998697723d4688c4
hseong3243 <hseong3243@gmail.com>
2023-11-01 23:05:25 +0900

    feat: 봉사 모집 요청 dto 검증 메시지를 추가한다.

--- src/main/java/com/clova/anifriends/domain/recruitment/dto/RegisterRecruitmentRequest.java
+++ src/main/java/com/clova/anifriends/domain/recruitment/dto/RegisterRecruitmentRequest.java
@@ -6,17 +6,17 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record RegisterRecruitmentRequest(
-    @NotBlank
+    @NotBlank(message = "봉사 모집글 제목은 필수값입니다.")
     String title,
-    @NotNull
+    @NotNull(message = "봉사 시작 시간은 필수값입니다.")
     LocalDateTime startTime,
-    @NotNull
+    @NotNull(message = "봉사 종료 시간은 필수값입니다.")
     LocalDateTime endTime,
-    @NotNull
+    @NotNull(message = "봉사 모집 마갑 시간은 필수값입니다.")
     LocalDateTime deadline,
-    @NotNull
+    @NotNull(message = "봉사 정원은 필수값입니다.")
     Integer capacity,
-    @NotBlank
+    @NotBlank(message = "봉사 모집글 본문은 필수값입니다.")
     String content,
     List<String> imageUrls) {
 
