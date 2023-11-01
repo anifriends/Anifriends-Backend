2b3812528f4d91248e8925fee88729321985b2c5
hseong3243 <hseong3243@gmail.com>
2023-11-02 01:26:00 +0900

    feat: Animal 검증 로직을 추가한다.

--- src/main/java/com/clova/anifriends/domain/animal/wrapper/AnimalBreed.java
+++ src/main/java/com/clova/anifriends/domain/animal/wrapper/AnimalBreed.java
@@ -1,7 +1,10 @@
 package com.clova.anifriends.domain.animal.wrapper;
 
+import com.clova.anifriends.domain.animal.exception.AnimalBadRequestException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import java.text.MessageFormat;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +14,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AnimalBreed {
 
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 20;
+
     @Column(name = "breed")
     private String breed;
 
     public AnimalBreed(String value) {
+        validateNotNull(value);
+        validateLength(value);
         this.breed = value;
+    }
+
+    private void validateNotNull(String value) {
+        if (Objects.isNull(value)) {
+            throw new AnimalBadRequestException("품종은 필수값입니다.");
+        }
+    }
+
+    private void validateLength(String value) {
+        if (value.length() < MIN_LENGTH || value.length() > MAX_LENGTH) {
+            throw new AnimalBadRequestException(
+                MessageFormat.format("품종은 {0}자 이상, {1}자 이하여야 합니다.", MIN_LENGTH, MAX_LENGTH));
+        }
     }
 }
--- src/main/java/com/clova/anifriends/domain/animal/wrapper/AnimalInformation.java
+++ src/main/java/com/clova/anifriends/domain/animal/wrapper/AnimalInformation.java
@@ -1,7 +1,10 @@
 package com.clova.anifriends.domain.animal.wrapper;
 
+import com.clova.anifriends.domain.animal.exception.AnimalBadRequestException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import java.text.MessageFormat;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +14,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AnimalInformation {
 
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 1000;
+
     @Column(name = "information")
     private String information;
 
     public AnimalInformation(String value) {
+        validateNotNull(value);
+        validateLength(value);
         this.information = value;
+    }
+
+    private void validateNotNull(String value) {
+        if (Objects.isNull(value)) {
+            throw new AnimalBadRequestException("기타 정보는 필수값입니다.");
+        }
+    }
+
+    private void validateLength(String value) {
+        if (value.length() < MIN_LENGTH || value.length() > MAX_LENGTH) {
+            throw new AnimalBadRequestException(
+                MessageFormat.format("기타 정보는 {0}자 이상, {1}자 이하여야 합니다.", MIN_LENGTH, MAX_LENGTH));
+        }
     }
 }
