5bffcfba34a9379e8500817f681484f07541825c
Sunny <rosa7536@naver.com>
2023-11-01 23:57:01 +0900

    feat: 봉사 신청 서비스단을 작성한다.

--- src/main/java/com/clova/anifriends/domain/applicant/repository/ApplicantRepository.java
+++ src/main/java/com/clova/anifriends/domain/applicant/repository/ApplicantRepository.java
@@ -1,8 +1,11 @@
 package com.clova.anifriends.domain.applicant.repository;
 
 import com.clova.anifriends.domain.applicant.Applicant;
+import com.clova.anifriends.domain.recruitment.Recruitment;
+import com.clova.anifriends.domain.volunteer.Volunteer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
 
+    boolean existsByRecruitmentAndVolunteer(Recruitment recruitment, Volunteer volunteer);
 }
