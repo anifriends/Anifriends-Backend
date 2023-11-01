247d11fee158bdc7886ca63292ddcd545280ffec
Sunny <rosa7536@naver.com>
2023-11-01 10:47:41 +0900

    feat: 봉사자 마이페이지 조회 controller단을 작성한다.

--- src/main/java/com/clova/anifriends/domain/volunteer/controller/VolunteerController.java
+++ src/main/java/com/clova/anifriends/domain/volunteer/controller/VolunteerController.java
@@ -1,11 +1,13 @@
 package com.clova.anifriends.domain.volunteer.controller;
 
 import com.clova.anifriends.domain.volunteer.dto.request.RegisterVolunteerRequest;
+import com.clova.anifriends.domain.volunteer.dto.response.GetVolunteerMyPageResponse;
 import com.clova.anifriends.domain.volunteer.service.VolunteerService;
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/volunteers")
+@RequestMapping("/api")
 public class VolunteerController {
 
     private final VolunteerService volunteerService;
     private static final String BASE_URI = "/api/volunteers/";
 
-    @PostMapping
+    @PostMapping("/volunteers")
     public ResponseEntity<Void> registerVolunteer(
         @RequestBody @Valid RegisterVolunteerRequest registerVolunteerRequest
     ) {
@@ -28,4 +30,10 @@ public class VolunteerController {
         return ResponseEntity.created(location).build();
     }
 
+    @GetMapping("/volunteers/me")
+    public ResponseEntity<GetVolunteerMyPageResponse> getVolunteerMyPage(
+        Long volunteerId // @UserId로 대체해야 함!
+    ) {
+        return ResponseEntity.ok(volunteerService.getVolunteerMyPage(volunteerId));
+    }
 }
