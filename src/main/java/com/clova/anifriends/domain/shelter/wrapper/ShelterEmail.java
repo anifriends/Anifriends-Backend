08a28321163d69d16b47b70297f4d3d9a03fc616
baejunil <bjo6300@naver.com>
2023-11-01 21:21:51 +0900

    feat: ShelterEmail을 검증한다.

--- src/main/java/com/clova/anifriends/domain/shelter/wrapper/ShelterEmail.java
+++ src/main/java/com/clova/anifriends/domain/shelter/wrapper/ShelterEmail.java
@@ -1,7 +1,10 @@
 package com.clova.anifriends.domain.shelter.wrapper;
 
+import com.clova.anifriends.domain.shelter.exception.ShelterBadRequestException;
+import com.clova.anifriends.global.exception.ErrorCode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import java.util.regex.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +14,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ShelterEmail {
 
+    private static final Pattern EMAIL_PATTERN =
+        Pattern.compile("^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]{1,64}@([a-zA-Z0-9.-]{1,255})$");
+
     @Column(name = "email")
     private String email;
 
     public ShelterEmail(String value) {
+        validateEmail(value);
         this.email = value;
+    }
+
+    private void validateEmail(String email) {
+        if (email == null || email.isBlank()) {
+            throw new ShelterBadRequestException(ErrorCode.BAD_REQUEST, "이메일은 필수 항목입니다.");
+        }
+
+        if (!EMAIL_PATTERN.matcher(email).matches()) {
+            throw new ShelterBadRequestException(ErrorCode.BAD_REQUEST, "잘못된 이메일 입력값입니다.");
+        }
     }
 }
