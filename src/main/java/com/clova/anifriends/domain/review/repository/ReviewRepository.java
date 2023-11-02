d472f864d4677d5c5be72a1cc75141679c6a32c7
hseong3243 <hseong3243@gmail.com>
2023-11-02 18:49:43 +0900

    fix: `@Query` 메서드의 파라미터에 `@Param` 추가

--- src/main/java/com/clova/anifriends/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/clova/anifriends/domain/review/repository/ReviewRepository.java
@@ -4,9 +4,12 @@ import com.clova.anifriends.domain.review.Review;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query("select r from Review r where r.reviewId = :reviewId and r.volunteer.volunteerId = :userId")
-    Optional<Review> findByReviewIdAndVolunteerId(Long reviewId, Long userId);
+    Optional<Review> findByReviewIdAndVolunteerId(
+        @Param("reviewId") Long reviewId,
+        @Param("userId") Long userId);
 }
