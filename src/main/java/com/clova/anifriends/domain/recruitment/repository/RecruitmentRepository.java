85b26047188b832d7f4f13be894f7dc9efbf2e83
pushedrumex <pushedrumex@naver.com>
2023-11-03 21:51:20 +0900

    feat: 사용하지 않는 메서드를 삭제한다.

--- src/main/java/com/clova/anifriends/domain/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/com/clova/anifriends/domain/recruitment/repository/RecruitmentRepository.java
@@ -1,15 +1,9 @@
 package com.clova.anifriends.domain.recruitment.repository;
 
 import com.clova.anifriends.domain.recruitment.Recruitment;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long>,
     RecruitmentRepositoryCustom {
 
-    @Query("select r from Recruitment r where r.shelter.shelterId = :shelterId and r.recruitmentId = :recruitmentId")
-    Optional<Recruitment> findByShelterIdAndRecruitmentId(@Param("shelterId") Long shelterId,
-        @Param("recruitmentId") Long recruitmentId);
 }
