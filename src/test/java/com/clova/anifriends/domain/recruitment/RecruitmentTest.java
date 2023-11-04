b1392f8d958b5dc13140614d9e3ee5a06dd21d5f
hseong3243 <hseong3243@gmail.com>
2023-11-04 21:24:52 +0900

    feat: Recruitment 생성 시 이미지 url 리스트 검증 로직을 추가한다.

--- src/test/java/com/clova/anifriends/domain/recruitment/RecruitmentTest.java
+++ src/test/java/com/clova/anifriends/domain/recruitment/RecruitmentTest.java
@@ -1,7 +1,11 @@
 package com.clova.anifriends.domain.recruitment;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchException;
 
+import com.clova.anifriends.domain.recruitment.exception.RecruitmentBadRequestException;
+import com.clova.anifriends.domain.shelter.Shelter;
+import com.clova.anifriends.domain.shelter.support.ShelterFixture;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -14,22 +18,23 @@ class RecruitmentTest {
     @DisplayName("Recruitment 생성 시")
     class NewRecruitmentTest {
 
+        Shelter shelter = ShelterFixture.shelter();
+        String title = "title";
+        int capacity = 10;
+        String content = "content";
+        LocalDateTime now = LocalDateTime.now();
+        List<String> imageUrls = List.of("imageUrl1", "imageUrl2");
+        LocalDateTime startTime = now.plusHours(1);
+        LocalDateTime endTime = startTime.plusMinutes(1);
+        LocalDateTime deadline = now.plusMinutes(10);
+
         @Test
         @DisplayName("성공")
         void newRecruitment() {
             //given
-            String title = "title";
-            int capacity = 10;
-            String content = "content";
-            LocalDateTime now = LocalDateTime.now();
-            List<String> imageUrls = List.of("imageUrl1", "imageUrl2");
-            LocalDateTime startTime = now.plusHours(1);
-            LocalDateTime endTime = startTime.plusMinutes(1);
-            LocalDateTime deadline = now.plusMinutes(10);
-
             //when
             Recruitment recruitment = new Recruitment(
-                null, title, capacity, content, startTime, endTime, deadline, imageUrls);
+                shelter, title, capacity, content, startTime, endTime, deadline, imageUrls);
 
             //then
             assertThat(recruitment.getTitle()).isEqualTo(title);
@@ -39,6 +44,21 @@ class RecruitmentTest {
             assertThat(recruitment.getEndTime()).isEqualTo(endTime);
             assertThat(recruitment.getDeadline()).isEqualTo(deadline);
             assertThat(recruitment.isClosed()).isFalse();
+        }
+
+        @Test
+        @DisplayName("예외(RecruitmentBadRequestException): 이미지 url 리스트 사이즈가 5를 초과")
+        void exceptionWhenImageUrlsSizeOver5() {
+            //given
+            List<String> imageUrlsOver5 = List.of("www.aws.s3.com/2", "www.aws.s3.com/2",
+                "www.aws.s3.com/2", "www.aws.s3.com/2", "www.aws.s3.com/2", "www.aws.s3.com/2");
+
+            //when
+            Exception exception = catchException(() -> new Recruitment(
+                shelter, title, capacity, content, startTime, endTime, deadline, imageUrlsOver5));
+
+            //then
+            assertThat(exception).isInstanceOf(RecruitmentBadRequestException.class);
         }
     }
 }
