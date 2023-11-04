67126517f81577588b083e7b7c097272fc2923fa
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2023-11-05 00:52:07 +0900

    feat: 봉사 후기 작성시 보호소 정보 조회 로직을 구현한다. (#72)
    * feat: 봉사 후기 작성시 보호소 정보 조회 서비스 구현한다.
    
    * test: 봉사 후기 작성시 보호소 정보 조회 서비스 테스트 코드를 작성한다.
    
    * feat: 봉사 후기 작성시 보호소 정보 조회 컨트롤러를 구현한다.
    
    * test: feat: 봉사 후기 작성시 보호소 정보 조회 컨트롤러 테스트 코드를 작성한다.
    
    * style: findShelterByVolunteerReview 첫 자를 소문자로 수정한다.
    
    * style: 재사용을 위해 FindShelterByVolunteerReview 이름을 findShelterSimple로 변경한다.

--- src/main/java/com/clova/anifriends/domain/recruitment/service/RecruitmentService.java
+++ src/main/java/com/clova/anifriends/domain/recruitment/service/RecruitmentService.java
@@ -8,6 +8,7 @@ import com.clova.anifriends.domain.recruitment.dto.response.FindRecruitmentByShe
 import com.clova.anifriends.domain.recruitment.dto.response.FindRecruitmentDetailByVolunteerResponse;
 import com.clova.anifriends.domain.recruitment.dto.response.FindRecruitmentsByShelterResponse;
 import com.clova.anifriends.domain.recruitment.dto.response.FindRecruitmentsByVolunteerResponse;
+import com.clova.anifriends.domain.recruitment.dto.response.FindShelterSimpleResponse;
 import com.clova.anifriends.domain.recruitment.dto.response.RegisterRecruitmentResponse;
 import com.clova.anifriends.domain.recruitment.exception.RecruitmentNotFoundException;
 import com.clova.anifriends.domain.recruitment.mapper.RecruitmentMapper;
@@ -59,7 +60,8 @@ public class RecruitmentService {
             pageable
         );
 
-        return FindRecruitmentsByShelterResponse.of(pagination.getContent(), PageInfo.from(pagination));
+        return FindRecruitmentsByShelterResponse.of(pagination.getContent(),
+            PageInfo.from(pagination));
     }
 
     private Shelter getShelterById(Long shelterId) {
@@ -75,6 +77,15 @@ public class RecruitmentService {
     public FindRecruitmentDetailByVolunteerResponse findRecruitmentByIdByVolunteer(long id) {
         Recruitment recruitment = getRecruitmentById(id);
         return FindRecruitmentDetailByVolunteerResponse.from(recruitment);
+    }
+
+    @Transactional(readOnly = true)
+    public FindShelterSimpleResponse findShelterSimple(
+        Long recruitmentId
+    ) {
+        Recruitment foundRecruitment = getRecruitmentById(recruitmentId);
+
+        return FindShelterSimpleResponse.from(foundRecruitment);
     }
 
     private Recruitment getRecruitmentById(long id) {
@@ -93,7 +104,8 @@ public class RecruitmentService {
 
     @Transactional(readOnly = true)
     public FindRecruitmentsByVolunteerResponse findRecruitmentsByVolunteer(
-        String keyword, LocalDate startDate, LocalDate endDate, Boolean isClosed, Boolean titleContains,
+        String keyword, LocalDate startDate, LocalDate endDate, Boolean isClosed,
+        Boolean titleContains,
         Boolean contentContains, Boolean shelterNameContains, Pageable pageable) {
         Page<Recruitment> recruitments = recruitmentRepository.findRecruitments(
             keyword,
