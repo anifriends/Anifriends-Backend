5288ded685227005347fc514bc10982732ba9010
baejunil <bjo6300@naver.com>
2023-11-04 17:49:15 +0900

    feat: 봉사자가 신청한 봉사 리스트 조회 컨트롤러 코드를 작성한다.

--- src/main/java/com/clova/anifriends/domain/applicant/controller/ApplicantController.java
+++ src/main/java/com/clova/anifriends/domain/applicant/controller/ApplicantController.java
@@ -1,9 +1,11 @@
 package com.clova.anifriends.domain.applicant.controller;
 
+import com.clova.anifriends.domain.applicant.dto.FindApplyingVolunteersResponse;
 import com.clova.anifriends.domain.applicant.service.ApplicantService;
 import com.clova.anifriends.domain.auth.resolver.LoginUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +25,12 @@ public class ApplicantController {
     ) {
         applicantService.registerApplicant(recruitmentId, volunteerId);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/volunteers/applicants")
+    public ResponseEntity<FindApplyingVolunteersResponse> findApplyingVolunteers(
+        @LoginUser Long volunteerId
+    ) {
+        return ResponseEntity.ok(applicantService.findApplyingVolunteers(volunteerId));
     }
 }
