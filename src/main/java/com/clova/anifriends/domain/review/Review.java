0a6a6361b34784e47da46d3ed49c2381ec667348
hseong3243 <48748265+hseong3243@users.noreply.github.com>
2023-11-06 12:06:18 +0900

    fix: 보호소에 달린 후기 리스트 조회 api 에서 봉사자의 리뷰 개수를 잘못 가져오는 로직을 수정한다. (#101)
    * fix: 봉사자의 리뷰 개수를 가져오는 로직을 수정한다.
    
    * refactor: Volunteer 클래스 메서드 순서 변경

--- src/main/java/com/clova/anifriends/domain/review/Review.java
+++ src/main/java/com/clova/anifriends/domain/review/Review.java
@@ -49,6 +49,7 @@ public class Review extends BaseTimeEntity {
         validateApplicant(applicant);
         validateImageUrlsSize(imageUrls);
         this.applicant = applicant;
+        this.applicant.registerReview(this);
         this.content = new ReviewContent(content);
         this.imageUrls = imageUrls == null ? null : imageUrls.stream()
             .map(url -> new ReviewImage(this, url))
