aa2787edf999632376ee0368d1c9b9b5623dd212
hseong3243 <hseong3243@gmail.com>
2023-11-07 15:42:19 +0900

    feat: 보호소 회원가입 서비스 로직 추가

--- src/main/java/com/clova/anifriends/domain/shelter/service/ShelterService.java
+++ src/main/java/com/clova/anifriends/domain/shelter/service/ShelterService.java
@@ -3,9 +3,12 @@ package com.clova.anifriends.domain.shelter.service;
 import com.clova.anifriends.domain.shelter.Shelter;
 import com.clova.anifriends.domain.shelter.dto.FindShelterDetailResponse;
 import com.clova.anifriends.domain.shelter.dto.FindShelterMyPageResponse;
+import com.clova.anifriends.domain.shelter.exception.ShelterBadRequestException;
 import com.clova.anifriends.domain.shelter.exception.ShelterNotFoundException;
 import com.clova.anifriends.domain.shelter.repository.ShelterRepository;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,7 +16,52 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ShelterService {
 
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MAX_PASSWORD_LENGTH = 16;
+
     private final ShelterRepository shelterRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public Long registerShelter(
+        String email,
+        String password,
+        String name,
+        String address,
+        String addressDetail,
+        String phoneNumber,
+        String sparePhoneNumber,
+        boolean isOpenedAddress) {
+        Shelter shelter = new Shelter(
+            email,
+            encodePassword(password),
+            address,
+            addressDetail,
+            name,
+            phoneNumber,
+            sparePhoneNumber,
+            isOpenedAddress);
+        shelterRepository.save(shelter);
+        return shelter.getShelterId();
+    }
+
+    private String encodePassword(String password) {
+        validatePasswordNotNull(password);
+        validatePasswordLength(password);
+        return passwordEncoder.encode(password);
+    }
+
+    private void validatePasswordNotNull(String password) {
+        if(Objects.isNull(password)) {
+            throw new ShelterBadRequestException("패스워드는 필수값입니다.");
+        }
+    }
+
+    private void validatePasswordLength(String password) {
+        if(password.length() < MIN_PASSWORD_LENGTH || password.length() > MAX_PASSWORD_LENGTH) {
+            throw new ShelterBadRequestException("패스워드는 6자 이상, 16자 이하여야 합니다.");
+        }
+    }
 
     @Transactional(readOnly = true)
     public FindShelterDetailResponse findShelterDetail(
