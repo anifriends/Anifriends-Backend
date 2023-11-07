cb8d9195fdb721bbfd444800a8b3fa57c09d43f5
baejunil <bjo6300@naver.com>
2023-11-07 23:21:49 +0900

    test: 보호소 입장에서의 보호 동물 조회 & 검색 레포지토리 테스트 코드를 추가한다.

--- src/test/java/com/clova/anifriends/domain/animal/repository/AnimalRepositoryImplTest.java
+++ src/test/java/com/clova/anifriends/domain/animal/repository/AnimalRepositoryImplTest.java
@@ -131,5 +131,49 @@ class AnimalRepositoryImplTest extends BaseRepositoryTest {
             // then
             assertThat(expected.getTotalElements()).isEqualTo(1);
         }
+
+        @Test
+        @DisplayName("성공: 이름, 종류, 성별, 중성화 여부가 일치")
+        void findAnimalsWhenNameAndTypeAndGenderAndIsNeuteredAreSame() {
+            // given
+            Shelter shelter = ShelterFixture.shelter();
+            ReflectionTestUtils.setField(shelter, "shelterId", 1L);
+            Animal animal = AnimalFixture.animal(shelter);
+            Animal animalNotFound = new Animal(
+                shelter,
+                "animalName",
+                LocalDate.now().minusMonths(1),
+                AnimalType.DOG.getName(),
+                "animalBreed",
+                AnimalGender.MALE.getName(),
+                true,
+                AnimalActive.ACTIVE.getName(),
+                14,
+                "animalInformation",
+                List.of("www.aws.s3.com/2", "www.aws.s3.com/2")
+            );
+
+            PageRequest pageRequest = PageRequest.of(0, 10);
+
+            shelterRepository.save(shelter);
+            animalRepository.save(animal);
+            animalRepository.save(animalNotFound);
+
+            // when
+            Page<Animal> expected = customAnimalRepository.findAnimalsByShelter(
+                shelter.getShelterId(),
+                "animalName",
+                AnimalType.DOG,
+                AnimalGender.FEMALE,
+                true,
+                null,
+                null,
+                null,
+                pageRequest
+            );
+
+            // then
+            assertThat(expected.getTotalElements()).isEqualTo(1);
+        }
     }
 }
