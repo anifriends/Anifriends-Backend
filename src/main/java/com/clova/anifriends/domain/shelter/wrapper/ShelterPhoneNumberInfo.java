d95b77aeffab5fe1ad1c37c8c4422ac9bdbbb344
hseong3243 <hseong3243@gmail.com>
2023-11-07 16:01:42 +0900

    refactor: 보호소 전화번호 검증 로직을 개선한다.

--- src/main/java/com/clova/anifriends/domain/shelter/wrapper/ShelterPhoneNumberInfo.java
+++ src/main/java/com/clova/anifriends/domain/shelter/wrapper/ShelterPhoneNumberInfo.java
@@ -1,11 +1,10 @@
 package com.clova.anifriends.domain.shelter.wrapper;
 
-import static java.util.Objects.isNull;
-
 import com.clova.anifriends.domain.shelter.exception.ShelterBadRequestException;
 import com.clova.anifriends.global.exception.ErrorCode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import java.util.Objects;
 import java.util.regex.Pattern;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -26,24 +25,25 @@ public class ShelterPhoneNumberInfo {
     private String sparePhoneNumber;
 
     public ShelterPhoneNumberInfo(String phoneNumber, String sparePhoneNumber) {
-        validatePhoneNumber(phoneNumber, sparePhoneNumber);
+        validateNotNull(phoneNumber, sparePhoneNumber);
+        validatePhoneNumberPattern(phoneNumber, sparePhoneNumber);
         this.phoneNumber = phoneNumber;
         this.sparePhoneNumber = sparePhoneNumber;
     }
 
-    private void validatePhoneNumber(String phoneNumber, String sparePhoneNumber) {
-        if (isNull(phoneNumber) || phoneNumber.isBlank()) {
-            throw new ShelterBadRequestException(ErrorCode.BAD_REQUEST, "전화번호는 필수 항목입니다.");
+    private void validateNotNull(String phoneNumber, String sparePhoneNumber) {
+        if(Objects.isNull(phoneNumber)) {
+            throw new ShelterBadRequestException("전화번호는 필수 항목입니다.");
         }
+        if(Objects.isNull(sparePhoneNumber)) {
+            throw new ShelterBadRequestException("임시 전화번호는 필수 항목입니다.");
+        }
+    }
 
+    private void validatePhoneNumberPattern(String phoneNumber, String sparePhoneNumber) {
         if (!PHONE_NUMBER_PATTERN.matcher(phoneNumber).matches()) {
             throw new ShelterBadRequestException(ErrorCode.BAD_REQUEST, "잘못된 전화번호 입력값입니다.");
         }
-
-        if (isNull(sparePhoneNumber) || sparePhoneNumber.isBlank()) {
-            throw new ShelterBadRequestException(ErrorCode.BAD_REQUEST, "임시 전화번호는 필수 항목입니다.");
-        }
-
         if (!PHONE_NUMBER_PATTERN.matcher(sparePhoneNumber).matches()) {
             throw new ShelterBadRequestException(ErrorCode.BAD_REQUEST, "잘못된 임시 전화번호 입력값입니다.");
         }
