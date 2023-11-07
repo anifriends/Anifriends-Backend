30c274971a8ea4afb6b47b8c41316bebc4180bdd
baejunil <bjo6300@naver.com>
2023-11-07 17:00:26 +0900

    feat: 보호소 입장에서의 보호 동물 조회 & 검색 컨트롤러를 구현한다.

---
+++ src/main/java/com/clova/anifriends/domain/animal/dto/request/FindAnimalsByShelterRequest.java
@@ -0,0 +1,36 @@
+package com.clova.anifriends.domain.animal.dto.request;
+
+import com.clova.anifriends.domain.animal.AnimalAge;
+import com.clova.anifriends.domain.animal.AnimalSize;
+import com.clova.anifriends.domain.animal.wrapper.AnimalActive;
+import com.clova.anifriends.domain.animal.wrapper.AnimalGender;
+import com.clova.anifriends.domain.animal.wrapper.AnimalType;
+
+public record FindAnimalsByShelterRequest(
+    String keyword,
+    AnimalType type,
+    AnimalGender gender,
+    Boolean isNeutered,
+    AnimalActive active,
+    AnimalSize size,
+    AnimalAge age
+) {
+
+    public FindAnimalsByShelterRequest(
+        String keyword,
+        AnimalType type,
+        AnimalGender gender,
+        Boolean isNeutered,
+        AnimalActive active,
+        AnimalSize size,
+        AnimalAge age
+    ) {
+        this.keyword = keyword;
+        this.type = type;
+        this.gender = gender;
+        this.isNeutered = isNeutered == null || isNeutered;
+        this.active = active;
+        this.size = size;
+        this.age = age;
+    }
+}
