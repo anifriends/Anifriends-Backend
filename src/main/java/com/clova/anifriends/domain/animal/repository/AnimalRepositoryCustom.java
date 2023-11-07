c356d89b2724c78bcc765ec4d2069b855e4d24f2
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2023-11-07 23:56:59 +0900

    feat: 보호소 입장에서 보호 동물 조회 & 검색 로직을 구현한다. (#143)
    * feat: 보호 동물 검색 필터에 맞는 Enum 값을 구현한다.
    
    * feat: 보호소 입장에서의 보호 동물 조회 & 검색 쿼리를 구현한다.
    
    * feat: 보호소 입장에서의 보호 동물 조회 & 검색 서비스 로직을 구현한다.
    
    * feat: 보호소 입장에서의 보호 동물 조회 & 검색 컨트롤러를 구현한다.
    
    * refactor: 보호소 입장에서의 보호 동물 조회 & 검색 쿼리 메서드의 null 값을 검사한다.
    
    * refactor: 보호소 입장에서의 보호 동물 조회 & 검색 쿼리 메서드 중 나이 검색 로직을 변경한다.
    
    * test: 보호소 입장에서의 보호 동물 조회 & 검색 레포지토리 테스트 코드를 작성한다.
    
    * test: 보호소 입장에서의 보호 동물 조회 & 검색 서비스 테스트 코드를 작성한다.
    
    * test: 보호소 입장에서의 보호 동물 조회 & 검색 컨트롤러 테스트 코드를 작성한다.
    
    * test: 보호소 입장에서의 보호 동물 조회 & 검색 레포지토리 테스트 코드를 추가한다.
    
    * fix: FindAnimalsByShelterRequest에서 불필요한 생성자를 제거한다.

---
+++ src/main/java/com/clova/anifriends/domain/animal/repository/AnimalRepositoryCustom.java
@@ -0,0 +1,25 @@
+package com.clova.anifriends.domain.animal.repository;
+
+import com.clova.anifriends.domain.animal.Animal;
+import com.clova.anifriends.domain.animal.AnimalAge;
+import com.clova.anifriends.domain.animal.AnimalSize;
+import com.clova.anifriends.domain.animal.wrapper.AnimalActive;
+import com.clova.anifriends.domain.animal.wrapper.AnimalGender;
+import com.clova.anifriends.domain.animal.wrapper.AnimalType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface AnimalRepositoryCustom {
+
+    Page<Animal> findAnimalsByShelter(
+        Long shelterId,
+        String keyword,
+        AnimalType type,
+        AnimalGender gender,
+        Boolean isNeutered,
+        AnimalActive active,
+        AnimalSize size,
+        AnimalAge age,
+        Pageable pageable
+    );
+}
