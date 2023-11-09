7c73a644a74db4999e6fbac044549a724c210f8b
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-11-09 16:25:35 +0900

    feat: 봉사 신청 현황 조회 로직을 구현한다. (#169)
    * feat: ApplicantRepository.findByRecruitmentIdAndShelterId 를 구현한다.
    
    * feat: ApplicantService.findApplicants 를 구현한다.
    
    * feat: ApplicantController.findApplicants 를 구현한다.

---
+++ src/main/java/com/clova/anifriends/domain/applicant/dto/FindApplicantsResponse.java
@@ -0,0 +1,44 @@
+package com.clova.anifriends.domain.applicant.dto;
+
+import com.clova.anifriends.domain.applicant.Applicant;
+import com.clova.anifriends.domain.recruitment.Recruitment;
+import java.time.LocalDate;
+import java.util.List;
+
+public record FindApplicantsResponse(
+    List<FindApplicantResponse> applicants,
+    Integer recruitmentCapacity
+) {
+
+    public record FindApplicantResponse(
+        Long volunteerId,
+        Long applicantId,
+        LocalDate volunteerBirthDate,
+        String volunteerGender,
+        Integer completedVolunteerCount,
+        Integer volunteerTemperature,
+        String applicantStatus
+    ) {
+
+        public static FindApplicantResponse from(Applicant applicant) {
+            return new FindApplicantResponse(
+                applicant.getVolunteer().getVolunteerId(),
+                applicant.getApplicantId(),
+                applicant.getVolunteer().getBirthDate(),
+                applicant.getVolunteer().getGender(),
+                applicant.getVolunteer().getApplicantCompletedCount(),
+                applicant.getVolunteer().getTemperature(),
+                applicant.getStatus().convertToApprovalStatus().name()
+            );
+        }
+    }
+
+    public static FindApplicantsResponse from(List<Applicant> applicants, Recruitment recruitment) {
+        return new FindApplicantsResponse(
+            applicants.stream()
+                .map(FindApplicantResponse::from)
+                .toList(),
+            recruitment.getCapacity()
+        );
+    }
+}
