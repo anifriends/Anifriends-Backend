685cd3a33f096a7d75a6130563704413891dd485
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-11-11 17:43:16 +0900

    feat: 봉사자 출석 상태 수정 로직을 구현한다. (#171)
    * feat: ApplicantRepository.updateBulkAttendance 를 구현한다.
    
    * feat: ApplicantService.updateApplicantAttendance 를 구현한다.
    
    * feat: ApplicantController.updateApplicantAttendance 를 구현한다.
    
    * feat: 불필요한 Applicant 생성자를 제거한다.
    
    * refactor: Applicant Status 변경 로직을 서비스 계층에서 분리한다.

--- src/main/java/com/clova/anifriends/domain/applicant/service/ApplicantService.java
+++ src/main/java/com/clova/anifriends/domain/applicant/service/ApplicantService.java
@@ -1,11 +1,13 @@
 package com.clova.anifriends.domain.applicant.service;
 
 import com.clova.anifriends.domain.applicant.Applicant;
-import com.clova.anifriends.domain.applicant.dto.FindApplicantsApprovedResponse;
 import com.clova.anifriends.domain.applicant.dto.FindApplicantsResponse;
-import com.clova.anifriends.domain.applicant.dto.FindApplyingVolunteersResponse;
+import com.clova.anifriends.domain.applicant.dto.response.FindApplicantsApprovedResponse;
+import com.clova.anifriends.domain.applicant.dto.response.FindApplyingVolunteersResponse;
 import com.clova.anifriends.domain.applicant.exception.ApplicantConflictException;
 import com.clova.anifriends.domain.applicant.repository.ApplicantRepository;
+import com.clova.anifriends.domain.applicant.service.dto.UpdateApplicantAttendanceCommand;
+import com.clova.anifriends.domain.applicant.wrapper.ApplicantStatus;
 import com.clova.anifriends.domain.recruitment.Recruitment;
 import com.clova.anifriends.domain.recruitment.exception.RecruitmentNotFoundException;
 import com.clova.anifriends.domain.recruitment.repository.RecruitmentRepository;
@@ -59,6 +61,7 @@ public class ApplicantService {
             .orElseThrow(() -> new VolunteerNotFoundException("존재하지 않는 봉사자입니다."));
     }
 
+    @Transactional(readOnly = true)
     public FindApplicantsApprovedResponse findApplicantsApproved(Long shelterId,
         Long recruitmentId) {
         List<Applicant> applicantsApproved = applicantRepository
@@ -66,10 +69,40 @@ public class ApplicantService {
         return FindApplicantsApprovedResponse.from(applicantsApproved);
     }
 
+    @Transactional(readOnly = true)
     public FindApplicantsResponse findApplicants(Long shelterId, Long recruitmentId) {
         Recruitment recruitment = getRecruitment(recruitmentId);
         List<Applicant> applicants = applicantRepository
             .findByRecruitmentIdAndShelterId(recruitmentId, shelterId);
         return FindApplicantsResponse.from(applicants, recruitment);
+    }
+
+    @Transactional
+    public void updateApplicantAttendance(Long shelterId, Long recruitmentId,
+        List<UpdateApplicantAttendanceCommand> applicantsCommand) {
+        updateToAttendance(shelterId, recruitmentId, applicantsCommand);
+        updateToNoShow(shelterId, recruitmentId, applicantsCommand);
+    }
+
+    private void updateToNoShow(Long shelterId, Long recruitmentId,
+        List<UpdateApplicantAttendanceCommand> applicantsCommand) {
+        List<Long> noShowIds = applicantsCommand.stream()
+            .filter(applicant -> !applicant.isAttended())
+            .map(UpdateApplicantAttendanceCommand::applicantId)
+            .toList();
+
+        applicantRepository.updateBulkAttendance(shelterId, recruitmentId, noShowIds,
+            ApplicantStatus.NO_SHOW);
+    }
+
+    private void updateToAttendance(Long shelterId, Long recruitmentId,
+        List<UpdateApplicantAttendanceCommand> applicantsCommand) {
+        List<Long> attendedIds = applicantsCommand.stream()
+            .filter(UpdateApplicantAttendanceCommand::isAttended)
+            .map(UpdateApplicantAttendanceCommand::applicantId)
+            .toList();
+
+        applicantRepository.updateBulkAttendance(shelterId, recruitmentId, attendedIds,
+            ApplicantStatus.ATTENDANCE);
     }
 }
