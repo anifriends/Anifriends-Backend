a4d58d6d32eecd140400233dc07f2f6620028bab
hseong3243 <hseong3243@gmail.com>
2023-11-13 18:40:12 +0900

    feat: 봉사 후기 삭제 api를 구현한다.

--- src/main/java/com/clova/anifriends/domain/review/controller/ReviewController.java
+++ src/main/java/com/clova/anifriends/domain/review/controller/ReviewController.java
@@ -13,6 +13,7 @@ import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -88,6 +89,14 @@ public class ReviewController {
     ) {
         reviewService.updateReview(volunteerId, reviewId, updateReviewRequest.content(),
             updateReviewRequest.imageUrls());
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/volunteers/reviews/{reviewId}")
+    public ResponseEntity<Void> deleteReview(
+        @LoginUser Long volunteerId,
+        @PathVariable("reviewId") Long reviewId) {
+        reviewService.deleteReview(volunteerId, reviewId);
         return ResponseEntity.noContent().build();
     }
 }
