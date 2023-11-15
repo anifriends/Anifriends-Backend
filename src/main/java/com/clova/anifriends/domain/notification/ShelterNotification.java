5cbd6f3e051b775cd5eec42fac68eff82008774a
Seonheui Jeon <88873302+funnysunny08@users.noreply.github.com>
2023-11-15 16:18:43 +0900

    feat: 알림 도메인을 구현한다.  (#218)
    * feat: 봉사자, 보호소 알림 도메인을 구현한다.
    
    * feat: 알림 관련 도메인 테스트 코드를 작성한다.
    
    * feat: 알림을 정의한다.
    
    * feat: 테스트에서 바뀐 알림 enum 명을 적용한다.
    
    * reefactor: 개행을 추가한다.

---
+++ src/main/java/com/clova/anifriends/domain/notification/ShelterNotification.java
@@ -0,0 +1,83 @@
+package com.clova.anifriends.domain.notification;
+
+import com.clova.anifriends.domain.common.BaseTimeEntity;
+import com.clova.anifriends.domain.notification.wrapper.NotificationContent;
+import com.clova.anifriends.domain.notification.wrapper.NotificationRead;
+import com.clova.anifriends.domain.notification.wrapper.NotificationTitle;
+import com.clova.anifriends.domain.notification.wrapper.NotificationType;
+import com.clova.anifriends.domain.shelter.Shelter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "shelter_notification")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ShelterNotification extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "shelter_notification_id")
+    private Long shelterNotificationId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shelter_id")
+    private Shelter shelter;
+
+    @Embedded
+    private NotificationTitle title;
+
+    @Embedded
+    private NotificationContent content;
+
+    @Embedded
+    private NotificationRead isRead = new NotificationRead(false);
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type")
+    private NotificationType type;
+
+    public ShelterNotification(
+        Shelter shelter,
+        String title,
+        String content,
+        String type
+    ) {
+        this.shelter = shelter;
+        this.title = new NotificationTitle(title);
+        this.content = new NotificationContent(content);
+        this.type = NotificationType.valueOf(type);
+    }
+
+    public Long getShelterNotificationId() {
+        return shelterNotificationId;
+    }
+
+    public Shelter getShelter() {
+        return shelter;
+    }
+
+    public String getTitle() {
+        return title.getTitle();
+    }
+
+    public String getContent() {
+        return content.getContent();
+    }
+
+    public Boolean getIsRead() {
+        return isRead.getIsRead();
+    }
+
+    public NotificationType getType() {
+        return type;
+    }
+}
