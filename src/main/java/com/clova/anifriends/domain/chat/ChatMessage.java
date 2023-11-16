6d58bec997140e4ce35de602bbba0a026562cd02
pushedrumex <pushedrumex@naver.com>
2023-11-16 22:09:43 +0900

    feat: ChatMessage 생성시 검증 로직을 구현한다.

--- src/main/java/com/clova/anifriends/domain/chat/ChatMessage.java
+++ src/main/java/com/clova/anifriends/domain/chat/ChatMessage.java
@@ -1,6 +1,7 @@
 package com.clova.anifriends.domain.chat;
 
 import com.clova.anifriends.domain.auth.jwt.UserRole;
+import com.clova.anifriends.domain.chat.exception.ChatMessageBadRequestException;
 import com.clova.anifriends.domain.common.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,6 +11,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.text.MessageFormat;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +23,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatMessage extends BaseTimeEntity {
 
+    public static final int MAX_MESSAGE_LENGTH = 1000;
     @Id
     @Column(name = "chat_message_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -42,10 +46,44 @@ public class ChatMessage extends BaseTimeEntity {
     private boolean isRead;
 
     public ChatMessage(ChatRoom chatRoom, Long senderId, UserRole senderRole, String message) {
+        validateChatRoom(chatRoom);
+        validateSenderId(senderId);
+        validateSenderRole(senderRole);
+        validateMessage(message);
+
         this.chatRoom = chatRoom;
         this.senderId = senderId;
         this.senderRole = senderRole;
         this.message = message;
         this.isRead = false;
+    }
+
+    private void validateMessage(String message) {
+        if (Objects.isNull(message) || message.isBlank()) {
+            throw new ChatMessageBadRequestException("메시지가 존재하지 않습니다.");
+        }
+        if (message.length() > MAX_MESSAGE_LENGTH) {
+            throw new ChatMessageBadRequestException(
+                MessageFormat.format("메시지는 {0}자 이하로 입력해주세요.", MAX_MESSAGE_LENGTH)
+            );
+        }
+    }
+
+    private void validateSenderRole(UserRole senderRole) {
+        if (Objects.isNull(senderRole)) {
+            throw new ChatMessageBadRequestException("메시지 전송자의 타입이 존재하지 않습니다.");
+        }
+    }
+
+    private void validateSenderId(Long senderId) {
+        if (Objects.isNull(senderId)) {
+            throw new ChatMessageBadRequestException("메시지 전송자의 아이디가 존재하지 않습니다.");
+        }
+    }
+
+    private void validateChatRoom(ChatRoom chatRoom) {
+        if (Objects.isNull(chatRoom)) {
+            throw new ChatMessageBadRequestException("채팅방이 존재하지 않습니다.");
+        }
     }
 }
