43d587e53baf1f15239c0f5dfdab8c31a26ae7a8
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2023-11-16 16:26:59 +0900

    feat: 채팅방 생성 기능을 구현한다. (#229)
    * feat: ChatRoom 생성자를 구현한다.
    
    * feat: ChatService.registerChatRoom 를 구현한다.
    
    * feat: ChatRoomController.registerChatRoom 를 구현한다.
    
    * refactor: 개행을 추가한다.

--- src/main/java/com/clova/anifriends/domain/chat/controller/ChatRoomController.java
+++ src/main/java/com/clova/anifriends/domain/chat/controller/ChatRoomController.java
@@ -1,13 +1,32 @@
 package com.clova.anifriends.domain.chat.controller;
 
+import com.clova.anifriends.domain.auth.LoginUser;
+import com.clova.anifriends.domain.chat.dto.request.RegisterChatRoomRequest;
 import com.clova.anifriends.domain.chat.service.ChatService;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class ChatRoomController {
 
     private final ChatService chatService;
+
+    @PostMapping("/volunteers/chat/rooms")
+    public ResponseEntity<Void> registerChatRoom(
+        @LoginUser Long volunteerId,
+        RegisterChatRoomRequest registerChatRoomRequest
+    ) {
+        long chatRoomId = chatService.registerChatRoom(volunteerId,
+            registerChatRoomRequest.shelterId());
+
+        URI location = URI.create("/api/volunteers/chat/rooms/" + chatRoomId);
+        return ResponseEntity.created(location).build();
+    }
 
 }
