c5b236c87c7453f84cd0a2ec5f3bbdc695b678f5
Sunny <rosa7536@naver.com>
2023-11-16 15:05:06 +0900

    feat: 보호소 알림 조회 API를 구현한다.

---
+++ src/main/java/com/clova/anifriends/domain/notification/dto/response/FindShelterNotificationsResponse.java
@@ -0,0 +1,37 @@
+package com.clova.anifriends.domain.notification.dto.response;
+
+import com.clova.anifriends.domain.notification.ShelterNotification;
+import com.clova.anifriends.domain.notification.wrapper.NotificationType;
+import java.util.List;
+
+public record FindShelterNotificationsResponse(
+    List<FindShelterNotification> notifications
+) {
+
+    private record FindShelterNotification(
+        Long notificationId,
+        String notificationTitle,
+        String notificationContent,
+        Boolean notificationIsRead,
+        NotificationType notificationType
+    ) {
+
+        private static FindShelterNotification from(ShelterNotification shelterNotification) {
+            return new FindShelterNotification(
+                shelterNotification.getShelterNotificationId(),
+                shelterNotification.getTitle(),
+                shelterNotification.getContent(),
+                shelterNotification.getIsRead(),
+                shelterNotification.getType()
+            );
+        }
+    }
+
+    public static FindShelterNotificationsResponse from(List<ShelterNotification> shelterNotifications) {
+        return new FindShelterNotificationsResponse(
+            shelterNotifications.stream()
+                .map(FindShelterNotification::from)
+                .toList()
+        );
+    }
+}
