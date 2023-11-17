d0018c7295e29d0554c768eede4304f7f3559242
hseong3243 <48748265+hseong3243@users.noreply.github.com>
2023-11-17 15:46:06 +0900

    feat: 채팅방 목록 조회 로직을 구현한다.(봉사자) (#234)
    * feat: 채팅방 목록 조회(봉사자) 서비스 로직, 쿼리를 구현한다.
    
    * feat: 채팅방 목록 조회(봉사자) api 구현한다.
    
    * docs: 채팅방 목록 조회(봉사자) api를 문서화한다.
    
    * test: 채팅방 목록 조회(봉사자) 서비스 로직, 쿼리 테스트를 추가한다.

--- src/main/java/com/clova/anifriends/domain/chat/service/ChatRoomService.java
+++ src/main/java/com/clova/anifriends/domain/chat/service/ChatRoomService.java
@@ -3,15 +3,18 @@ package com.clova.anifriends.domain.chat.service;
 import com.clova.anifriends.domain.auth.jwt.UserRole;
 import com.clova.anifriends.domain.chat.ChatRoom;
 import com.clova.anifriends.domain.chat.dto.response.FindChatRoomDetailResponse;
+import com.clova.anifriends.domain.chat.dto.response.FindChatRoomsResponse;
 import com.clova.anifriends.domain.chat.exception.ChatNotFoundException;
 import com.clova.anifriends.domain.chat.repository.ChatMessageRepository;
 import com.clova.anifriends.domain.chat.repository.ChatRoomRepository;
+import com.clova.anifriends.domain.chat.repository.response.FindChatRoomResult;
 import com.clova.anifriends.domain.shelter.Shelter;
 import com.clova.anifriends.domain.shelter.exception.ShelterNotFoundException;
 import com.clova.anifriends.domain.shelter.repository.ShelterRepository;
 import com.clova.anifriends.domain.volunteer.Volunteer;
 import com.clova.anifriends.domain.volunteer.exception.VolunteerNotFoundException;
 import com.clova.anifriends.domain.volunteer.repository.VolunteerRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -54,9 +57,16 @@ public class ChatRoomService {
             .orElseThrow(() -> new ShelterNotFoundException("보호소가 존재하지 않습니다."));
     }
 
+    @Transactional(readOnly = true)
+    public FindChatRoomsResponse findChatRoomsByVolunteer(Long volunteerId) {
+        Volunteer volunteer = getVolunteer(volunteerId);
+        List<FindChatRoomResult> findChatRoomResult
+            = chatRoomRepository.findChatRoomsByVolunteer(volunteer);
+        return ChatRoomMapper.toResponse(findChatRoomResult);
+    }
+
     private Volunteer getVolunteer(Long volunteerId) {
-        return volunteerRepository
-            .findById(volunteerId)
-            .orElseThrow(() -> new VolunteerNotFoundException("봉사자가 존재하지 않습니다."));
+        return volunteerRepository.findById(volunteerId)
+            .orElseThrow(() -> new VolunteerNotFoundException("존재하지 않는 봉사자입니다."));
     }
 }
