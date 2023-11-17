c8bc1860078f4f078d3ff5732b54699fb02e56df
Sunny <rosa7536@naver.com>
2023-11-17 15:37:47 +0900

    feat: updateBulkRead 쿼리를 작성하고 테스트한다.

--- src/main/java/com/clova/anifriends/domain/notification/repository/VolunteerNotificationRepository.java
+++ src/main/java/com/clova/anifriends/domain/notification/repository/VolunteerNotificationRepository.java
@@ -3,8 +3,16 @@ package com.clova.anifriends.domain.notification.repository;
 import com.clova.anifriends.domain.notification.VolunteerNotification;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VolunteerNotificationRepository extends JpaRepository<VolunteerNotification, Long> {
 
     List<VolunteerNotification> findByVolunteer_VolunteerIdOrderByCreatedAtDesc(Long volunteerId);
+
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("update VolunteerNotification v set v.isRead.isRead = true "
+        + "where v.volunteer.volunteerId = :volunteerId and v.isRead.isRead = false")
+    void updateBulkRead(@Param("volunteerId") Long volunteerId);
 }
