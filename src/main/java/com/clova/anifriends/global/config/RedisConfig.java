5c0c4dcc0bfbf3923e2b05c158a7f76a8309430b
배준일(Bae Junil) <70627982+bjo6300@users.noreply.github.com>
2023-11-23 14:21:35 +0900

    chore: dev 서버에 docker-compose를 이용한 redis 환경을 설정한다. (#300)
    * chore: redis 설정을 추가한다.
    
    * feat: redis 동작 확인을 위한 테스트 api를 추가한다.
    
    * feat: redis 테스트용 api를 제거한다.

---
+++ src/main/java/com/clova/anifriends/global/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.clova.anifriends.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@EnableCaching
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, Integer> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Integer> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericToStringSerializer<>(Integer.class));
+        return template;
+    }
+}
