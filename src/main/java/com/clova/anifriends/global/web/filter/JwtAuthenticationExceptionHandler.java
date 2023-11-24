e7ac927a382cb147ff237fad701f1ae452be9d52
hseong3243 <hseong3243@gmail.com>
2023-11-24 11:05:15 +0900

    test: JWT 인증 예외 핸들러 필터를 테스트한다.

--- src/main/java/com/clova/anifriends/global/web/filter/JwtAuthenticationExceptionHandler.java
+++ src/main/java/com/clova/anifriends/global/web/filter/JwtAuthenticationExceptionHandler.java
@@ -2,6 +2,7 @@ package com.clova.anifriends.global.web.filter;
 
 import com.clova.anifriends.domain.auth.exception.ExpiredAccessTokenException;
 import com.clova.anifriends.domain.auth.exception.InvalidJwtException;
+import com.clova.anifriends.global.exception.ErrorCode;
 import com.clova.anifriends.global.exception.ErrorResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
@@ -31,12 +32,12 @@ public class JwtAuthenticationExceptionHandler extends OncePerRequestFilter {
         } catch (ExpiredAccessTokenException e) {
             log.debug("[Authentication] JWT 인증 예외 핸들링. 만료된 JWT");
             ErrorResponse errorResponse
-                = new ErrorResponse(e.getErrorCode(), "액세스 토큰이 만료되었습니다.");
+                = new ErrorResponse(ErrorCode.ACCESS_TOKEN_EXPIRED.getValue(), "액세스 토큰이 만료되었습니다.");
             setResponse(response, errorResponse);
         } catch (InvalidJwtException e) {
             log.debug("[Authentication] JWT 인증 예외 핸들링. 잘못된 JWT");
             ErrorResponse errorResponse
-                = new ErrorResponse(e.getErrorCode(), "인증되지 않은 사용자의 요청입니다.");
+                = new ErrorResponse(ErrorCode.UN_AUTHENTICATION.getValue(), "인증되지 않은 사용자의 요청입니다.");
             setResponse(response, errorResponse);
         }
     }
