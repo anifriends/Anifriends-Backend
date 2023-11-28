3d9d6f577480b4e63b37a72c4ea6c5c75abc6468
baejunil <bjo6300@naver.com>
2023-11-28 15:00:32 +0900

    chore: https 테스트를 위해 주석처리한다.

--- src/main/java/com/clova/anifriends/global/event/CacheWarmer.java
+++ src/main/java/com/clova/anifriends/global/event/CacheWarmer.java
@@ -1,22 +1,22 @@
-package com.clova.anifriends.global.event;
-
-import com.clova.anifriends.domain.recruitment.service.RecruitmentCacheService;
-import com.clova.anifriends.domain.animal.repository.AnimalCacheRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
-
-@Component
-@RequiredArgsConstructor
-public class CacheWarmer {
-
-    private final RecruitmentCacheService recruitmentCacheService;
-    private final AnimalCacheRepository animalCacheRepository;
-
-    @EventListener(ApplicationReadyEvent.class)
-    void synchronizeCache() {
-        animalCacheRepository.synchronizeCache();
-        recruitmentCacheService.synchronizeCache();
-    }
-}
+//package com.clova.anifriends.global.event;
+//
+//import com.clova.anifriends.domain.recruitment.service.RecruitmentCacheService;
+//import com.clova.anifriends.domain.animal.repository.AnimalCacheRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.context.event.ApplicationReadyEvent;
+//import org.springframework.context.event.EventListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class CacheWarmer {
+//
+//    private final RecruitmentCacheService recruitmentCacheService;
+//    private final AnimalCacheRepository animalCacheRepository;
+//
+//    @EventListener(ApplicationReadyEvent.class)
+//    void synchronizeCache() {
+//        animalCacheRepository.synchronizeCache();
+//        recruitmentCacheService.synchronizeCache();
+//    }
+//}
