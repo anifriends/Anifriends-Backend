0c63c66c4f034d8a0c792370c7a7dc2d1b2d8d8d
sunny <rosa7536@naver.com>
2023-11-28 17:18:04 +0900

    feat: 봉사 3일전, 1일전 알림을 생성한다.

--- src/main/java/com/clova/anifriends/domain/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/com/clova/anifriends/domain/recruitment/repository/RecruitmentRepository.java
@@ -2,6 +2,8 @@ package com.clova.anifriends.domain.recruitment.repository;
 
 import com.clova.anifriends.domain.recruitment.Recruitment;
 import jakarta.persistence.LockModeType;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -41,4 +43,9 @@ public interface RecruitmentRepository
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select r from Recruitment r where r.recruitmentId = :recruitmentId")
     Optional<Recruitment> findByIdPessimistic(@Param("recruitmentId") Long recruitmentId);
+
+    @Query("select r from Recruitment r left join fetch r.applicants "
+        + "where r.info.startTime >= :time1 and r.info.startTime <= :time2")
+    List<Recruitment> findRecruitmentByStartTime(@Param("time1") LocalDateTime time1,
+        @Param("time2") LocalDateTime time2);
 }
