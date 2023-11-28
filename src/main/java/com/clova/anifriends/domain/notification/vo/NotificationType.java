1e520b4f0a2d927d9fce53711f9e8e49af6ecb5f
Seonheui Jeon <88873302+funnysunny08@users.noreply.github.com>
2023-11-28 23:40:36 +0900

    feat: 스케줄러 미사용 알림을 적용한다.  (#330)
    * feat: 새로운 application이 생성되면, shelter에 알림을 보낸다.
    
    * feat: 리뷰가 생성되면, shelter에 알림을 보낸다.
    
    * feat: 봉사 신청의 상태가 변경되면, 봉사자에게 수락/거절 알림을 보낸다.
    
    * feat: 출석 상태가 변경되면, 봉사자에게 출석 완료 알림을 보낸다.
    
    * feat: 모집에 대한 인원이 마감되면, shelter에 알림을 보낸다.

--- src/main/java/com/clova/anifriends/domain/notification/vo/NotificationType.java
+++ src/main/java/com/clova/anifriends/domain/notification/vo/NotificationType.java
@@ -5,9 +5,9 @@ import com.clova.anifriends.domain.common.EnumType;
 public enum NotificationType implements EnumType {
 
     // 보호소
-    NEW_APPLICANT("새로운 봉사 신청이 있습니다."),
-    APPLICANT_FULL("봉사 인원이 마감되었습니다."),
-    ENCOURAGE_CHECK_ATTENDANCE("봉사자 출석체크를 진행해주세요."),
+    NEW_APPLICANT("님께서 지원하셨습니다."),
+    APPLICANT_FULL("마감되었습니다."),
+    ENCOURAGE_CHECK_ATTENDANCE("봉사가 시작되었습니다. 출석 확인을 진행해주세요!"),
     NEW_SHELTER_REVIEW("보호소에 새로운 후기가 등록되었습니다."),
 
     // 봉사자
@@ -15,7 +15,7 @@ public enum NotificationType implements EnumType {
     VOLUNTEER_REFUSED("봉사 신청이 거절되었습니다."),
     A_DAY_BEFORE_VOLUNTEER("봉사 하루 전입니다."),
     THREE_DAY_BEFORE_VOLUNTEER("봉사 3일 전입니다."),
-    CONFIRMED_ATTENDANCE("봉사 출석이 확정되었습니다."),
+    INCREASE_VOLUNTEER_TEMPERATURE("'C의 봉사자 온도가 상승하였습니다."),
     ENCOURAGE_WRITE_REVIEW("봉사 후기를 작성해주세요.");
 
     private final String message;
