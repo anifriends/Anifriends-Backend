0dc27a54e8fd0345b2d02f532f4ec2696cb14f6b
pushedrumex <pushedrumex@naver.com>
2023-12-12 20:39:05 +0900

    test: ApiServiceTest 를 추가한다.

--- src/test/java/com/clova/anifriends/global/infrastructure/ApiServiceTest.java
+++ src/test/java/com/clova/anifriends/global/infrastructure/ApiServiceTest.java
@@ -1,9 +1,11 @@
 package com.clova.anifriends.global.infrastructure;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchException;
 import static org.springframework.http.HttpMethod.POST;
 
 import com.clova.anifriends.domain.payment.dto.response.TossPaymentApiResponse;
+import com.clova.anifriends.global.exception.ExternalApiException;
 import java.io.IOException;
 import net.minidev.json.JSONObject;
 import okhttp3.mockwebserver.MockResponse;
@@ -42,7 +44,7 @@ class ApiServiceTest {
     class PostTest {
 
         @Test
-        @DisplayName("외부 api 에 post 요청하여 응답을 받는다")
+        @DisplayName("성공: 외부 api 에 post 요청하여 응답을 받는다")
         void post() throws InterruptedException {
             // given
             String expectedJsonResponse = "{\"status\": \"DONE\"}";
@@ -76,6 +78,37 @@ class ApiServiceTest {
 
             assertThat(response).isNotNull();
             assertThat(response.status()).isEqualTo("DONE");
+        }
+
+        @Test
+        @DisplayName("예외(ExternalApiException): 상태 코드가 4xx 인 경우")
+        void exceptionWhenResponseIsNull() {
+            // given
+            String expectedJsonResponse = "{\"status\": \"DONE\"}";
+            String url = mockWebServer.url("/").toString();
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+            JSONObject params = new JSONObject();
+            params.put("paymentKey", "mockPaymentKey");
+            params.put("orderId", "mockOrderId");
+            params.put("amount", 10000);
+
+            mockWebServer.enqueue(
+                new MockResponse()
+                    .addHeader("Content-Type", "application/json")
+                    .setResponseCode(401)
+                    .setBody(expectedJsonResponse)
+            );
+
+            // when
+            Exception exception = catchException(() -> apiService.post(
+                new HttpEntity<>(params, httpHeaders),
+                url,
+                TossPaymentApiResponse.class));
+
+            // then
+            assertThat(exception).isInstanceOf(ExternalApiException.class);
         }
     }
 }
