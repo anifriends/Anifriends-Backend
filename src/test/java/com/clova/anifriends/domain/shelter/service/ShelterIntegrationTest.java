4c6891e0e7a0292f2052eee0eba2b943e59b4be8
pushedrumex <pushedrumex@naver.com>
2023-12-26 17:25:32 +0900

    test: 프로필 사진 수정 관련 테스트 코드를 작성한다.

--- src/test/java/com/clova/anifriends/domain/shelter/service/ShelterIntegrationTest.java
+++ src/test/java/com/clova/anifriends/domain/shelter/service/ShelterIntegrationTest.java
@@ -6,6 +6,7 @@ import com.clova.anifriends.base.BaseIntegrationTest;
 import com.clova.anifriends.domain.common.CustomPasswordEncoder;
 import com.clova.anifriends.domain.shelter.Shelter;
 import com.clova.anifriends.domain.shelter.dto.response.RegisterShelterResponse;
+import com.clova.anifriends.domain.shelter.support.ShelterFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -51,6 +52,41 @@ public class ShelterIntegrationTest extends BaseIntegrationTest {
             boolean isPasswordUpdated
                 = passwordEncoder.matchesPassword(newRawPassword, findShelter.getPassword());
             assertThat(isPasswordUpdated).isTrue();
+        }
+    }
+
+    @Nested
+    @DisplayName("updateShelterInfo 메서드 호출 시")
+    class UpdateShelterInfo {
+
+        @Test
+        @DisplayName("성공: 이미지 url 입력값이 들어왔고, 기존의 봉사자 이미지와 다르다면 shelterImage 가 새롭게 생성된다.")
+        void updateShelterInfoWhenDifferentFromCurrentImage() {
+            // given
+            Shelter shelter = ShelterFixture.shelter();
+            shelterRepository.save(shelter);
+
+            String beforeImage = "beforeImage.jpg";
+            String afterImage = "afterImage.jpg";
+
+            shelterService.updateShelter(shelter.getShelterId(), shelter.getName(),
+                beforeImage, shelter.getAddress(), shelter.getAddressDetail(),
+                shelter.getPhoneNumber(), shelter.getSparePhoneNumber(),
+                shelter.isOpenedAddress());
+
+            // when
+            shelterService.updateShelter(shelter.getShelterId(), shelter.getName(),
+                afterImage, shelter.getAddress(), shelter.getAddressDetail(),
+                shelter.getPhoneNumber(), shelter.getSparePhoneNumber(),
+                shelter.isOpenedAddress());
+
+            // then
+            Shelter updatedShelter = entityManager.createQuery(
+                    "select s from Shelter s join fetch s.image where s.shelterId = :shelterId",
+                    Shelter.class)
+                .setParameter("shelterId", shelter.getShelterId())
+                .getSingleResult();
+            assertThat(updatedShelter.getImage()).isEqualTo(afterImage);
         }
     }
 }
