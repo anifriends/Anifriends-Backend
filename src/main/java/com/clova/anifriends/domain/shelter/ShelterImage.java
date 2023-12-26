45e78e481bc6e565f3ee41fe30af26bcd16a700b
pushedrumex <pushedrumex@naver.com>
2023-12-26 17:24:08 +0900

    fix: oneToOne 관계에서 주테이블이 외래키를 갖도록 수정한다.

--- src/main/java/com/clova/anifriends/domain/shelter/ShelterImage.java
+++ src/main/java/com/clova/anifriends/domain/shelter/ShelterImage.java
@@ -3,11 +3,9 @@ package com.clova.anifriends.domain.shelter;
 import com.clova.anifriends.domain.common.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -25,8 +23,7 @@ public class ShelterImage extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long shelterImageId;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "shelter_id")
+    @OneToOne(mappedBy = "image")
     private Shelter shelter;
 
     @Column(name = "image_url")
