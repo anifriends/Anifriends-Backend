c1c9cb613c68b0426b16e62f0cca4cc02f36a6cd
pushedrumex <pushedrumex@naver.com>
2024-01-04 00:28:26 +0900

    feat: DonationRedisRepository 를 생성한다.

---
+++ src/main/java/com/clova/anifriends/domain/donation/repository/DonationRedisRepository.java
@@ -0,0 +1,29 @@
+package com.clova.anifriends.domain.donation.repository;
+
+import jakarta.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class DonationRedisRepository implements DonationCacheRepository {
+
+    public static final int TIMEOUT = 1;
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    private ValueOperations<String, Object> valueOperations;
+
+    @PostConstruct
+    public void init() {
+        valueOperations = redisTemplate.opsForValue();
+    }
+
+    public boolean isDuplicateDonation(Long volunteerId) {
+        String key = "donation:" + volunteerId;
+        return !valueOperations.setIfAbsent(key, true, TIMEOUT, TimeUnit.SECONDS);
+    }
+}
